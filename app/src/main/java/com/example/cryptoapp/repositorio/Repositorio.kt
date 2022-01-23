da6833ccd1cf1d7b643595f30db7275f8b999f3b
mlopezgue <mslopezgue@gmail.com>
2022-01-23 00:50:07 -0300

    Avance

--- app/src/main/java/com/example/cryptoapp/repositorio/Repositorio.kt
+++ app/src/main/java/com/example/cryptoapp/repositorio/Repositorio.kt
@@ -2,70 +2,79 @@ package com.example.cryptoapp.repositorio
 
 
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import com.example.cryptoapp.cliente.RetrofitHelper
+import com.example.cryptoapp.dao.DetalleMonedaEntity
 import com.example.cryptoapp.dao.MonedaDao
 import com.example.cryptoapp.dao.MonedaEntity
+import com.example.cryptoapp.database.CryptoApplication.Companion.db
+import com.example.cryptoapp.database.MonedaConverters
+import com.example.cryptoapp.model.DetalleMoneda
 import com.example.cryptoapp.model.Moneda
 import com.example.cryptoapp.network.MonedaService
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.launch
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 
-class Repositorio(private val monedaDao: MonedaDao) {
 
-    private val service =RetrofitHelper.obtenCliente()
-    var lista = MutableLiveData<List<Moneda>>()
+class Repositorio {
+    private val monedaService = MonedaService()
 
-    fun obtenerNoticias() {
-        val call = service.create()baseUrl()
-        call.enqueue(object : Callback<Moneda> {
-            override fun onResponse(call: Call<Moneda>, response: Response<Moneda>)
+    fun getAll(): Flow<List<Moneda>> = flow {
+    while(true)
+    {
+        val monedaResponse = kotlin.runCatching { monedaService.getMonedas() }
+
+        monedaResponse.onSuccess {
+            if(it.body()!=null)
             {
-                CoroutineScope(Dispatchers.IO).launch {
-                    response.body()?.let {
-                        Log.v("logenrepo", response.body().toString())
-                        monedaDao.agregar(it)
-                    }
-                }
+                db.monedaDAO().insertMonedas(MonedaConverters.converterMonedasEntity(it.body()!!))
             }
-            override fun onFailure(call: Call<Moneda>, t: Throwable) {
-                call.cancel()
-            }
-        })
-    }
-    fun traerDataBD(): LiveData<List<MonedaEntity>> {
-        return monedaDao.listar()
-    }
-    fun busquedaRepo(noticia: String, idioma: String, apikey: String) {
+        }
 
-        val call = service.buscarNoticias(noticia, idioma, apikey)
-        call.enqueue(object : Callback<NoticiaResponse> {
+        monedaResponse.onFailure {
+            Log.d("Error Moneda", it.toString())
+        }
 
-            override fun onResponse(call: Call<NoticiaResponse>, response: Response<NoticiaResponse>) {
-                CoroutineScope(Dispatchers.IO).launch {
-                    Log.v("busquedaRepo",lista .value.toString())
-                    lista.postValue(response.body()?.articles)
-                }
-            }
+        val monedasEntity: List<MonedaEntity> = db.monedaDAO().getAll()
 
-            override fun onFailure(call: Call<NoticiaResponse>, t: Throwable) {
-                call.cancel()
+        if(monedasEntity!=null)
+        {
+            emit(MonedaConverters.converterMonedas(monedasEntity))
+        }
 
-            }
-        })
-
+        delay(5000)
     }
 
-    fun mostrarBusquedaEnRepo(): MutableLiveData<List<Articulo>> {
-        Log.v("TraerBusquedaRepo", lista.toString())
-        return lista
-    }
-}
+}.flowOn(Dispatchers.IO)
 
+fun getDetailMoneda(id:String): Flow<DetalleMoneda> = flow {
+    while(true)
+    {
+        val monedaResponse = kotlin.runCatching { monedaService.getMoneda(id) }
+
+        monedaResponse.onSuccess {
+            Log.d("DetalleMoneda", it.body().toString())
+            if(it.body()!=null)
+            {
+                db.monedaDAO().insertDetailMoneda(MonedaConverters.converterDetailMonedaEntity(it.body()!!))
+            }
+        }
+
+        monedaResponse.onFailure {
+            Log.d("ErrorDetalleMoneda", it.toString())
+        }
+
+        val detailMonedaEntity: DetalleMonedaEntity = db.monedaDAO().getDetailMoneda(id)
+
+        if(detailMonedaEntity!=null)
+        {
+            emit(MonedaConverters.converterDetailMoneda(detailMonedaEntity))
+        }
+
+        delay(5000)
+    }
+
+}.flowOn(Dispatchers.IO)
 }
