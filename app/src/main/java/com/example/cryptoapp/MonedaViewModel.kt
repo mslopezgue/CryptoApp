f624712613fd11836658b743df2d674cb701f408
mlopezgue <mslopezgue@gmail.com>
2022-01-23 00:55:56 -0300

    Avance

--- app/src/main/java/com/example/cryptoapp/MonedaViewModel.kt
+++ app/src/main/java/com/example/cryptoapp/MonedaViewModel.kt
@@ -1,4 +1,12 @@
 package com.example.cryptoapp
 
-class MonedaViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.example.cryptoapp.database.CryptoApplication.Companion.prefs
+import com.example.cryptoapp.repositorio.Repositorio
+
+class MonedaViewModel : ViewModel() {
+    private val repository = Repositorio()
+    var monedas = repository.getAll().asLiveData()
+    var detailMoneda = repository.getDetailMoneda(prefs.idMoneda!!).asLiveData()
 }
